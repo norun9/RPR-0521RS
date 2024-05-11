4483fe800652b67b34fd7d814b94dc11d85c5f16
norun9 <u.t012807@gmail.com>
2024-05-11 14:38:56 +0900

    fix error handling

--- main/main.c
+++ main/main.c
@@ -2,14 +2,14 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 
-static const char *TAG = "RPR-0521";
+static const char *TAG = "RPR-0521rs";
 
 #define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM I2C_NUM_0                // I2Cポート番号
-#define I2C_MASTER_FREQ_HZ 400000               /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0             /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0             /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 400000   /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS 1000
 
 #define ALS_DATA0_LSB_REG 0x46 // ALS DATA0 測定結果(low byte)
@@ -18,7 +18,7 @@ static const char *TAG = "RPR-0521";
 
 static esp_err_t read_als_value(uint16_t *als_value)
 {
-    uint8_t data_rd[2] = {0}; // ALS DATA0 測定結果(low byte)とALS DATA0 測定結果(high byte)を格納するためのバッファ
+    uint8_t data_rd[2] = {0};
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (RPR0521RS_ADDR << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
@@ -32,25 +32,17 @@ static esp_err_t read_als_value(uint16_t *als_value)
 
     esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
-
-    if (ret != ESP_OK)
+    if (ret == ESP_OK)
     {
-        return ret;
+        uint8_t als_data0_lsb = data_rd[0];
+        uint8_t als_data0_msb = data_rd[1];
+        *als_value = ((uint16_t)als_data0_msb << 8) | als_data0_lsb;
     }
-
-    ESP_LOGI(TAG, "msb:%d, lsb:%d", data_rd[1], data_rd[0]);
-
-    *als_value = ((uint16_t)data_rd[1] << 8) | data_rd[0];
-    return ESP_OK;
+    return ret;
 }
 
-/**
- * @brief i2c master initialization
- */
 static esp_err_t i2c_master_init(void)
 {
-    int i2c_master_port = I2C_MASTER_NUM;
-
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -60,60 +52,60 @@ static esp_err_t i2c_master_init(void)
         .master.clk_speed = I2C_MASTER_FREQ_HZ,
     };
 
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    esp_err_t ret = i2c_param_config(I2C_MASTER_NUM, &conf);
+    if (ret != ESP_OK)
+        return ret;
+    return i2c_driver_install(I2C_MASTER_NUM, conf.mode, I2C_MASTER_TX_BUF_DISABLE, I2C_MASTER_RX_BUF_DISABLE, 0);
 }
 
-esp_err_t write_rpr0521_config(i2c_port_t i2c_num, uint8_t reg_addr, uint8_t value)
+esp_err_t configure_rpr0521rs(i2c_port_t i2c_num, uint8_t reg_addr, uint8_t value)
 {
-    esp_err_t ret;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (RPR0521RS_ADDR << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, value, ACK_CHECK_EN);
-    ret = i2c_master_cmd_begin(i2c_num, cmd, 10 / portTICK_PERIOD_MS);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
     return ret;
 }
 
 esp_err_t rpr0521_init(void)
 {
-    esp_err_t ret;
-    // setup MODE_CONTROL
-    ret = write_rpr0521_config(I2C_MASTER_NUM, 0x41, 0x8A);
-    if (ret != ESP_OK)
+    if (configure_rpr0521rs(I2C_MASTER_NUM, 0x41, 0x8A) != ESP_OK)
     {
-        ESP_LOGE(TAG, "MODE_CONTROL setting failed");
+        ESP_LOGE(TAG, "Failed to configure MODE_CONTROL");
+        return ESP_FAIL;
     }
-    // setup ALS_PS_CONTROL
-    // ret = setup_sensor_config(I2C_MASTER_NUM, 0x42, 0x02);
-    // if (ret != ESP_OK)
-    // {
-    //     ESP_LOGE(TAG, "ALS_PS_CONTROL setting failed");
-    // }
-    ESP_LOGI(TAG, "configuration initialized successfully");
-    return ret;
+    if (configure_rpr0521rs(I2C_MASTER_NUM, 0x42, 0x02) != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to configure ALS_PS_CONTROL");
+        return ESP_FAIL;
+    }
+    return ESP_OK;
 }
 
 void app_main(void)
 {
     ESP_ERROR_CHECK(i2c_master_init());
     ESP_LOGI(TAG, "I2C initialized successfully");
-
     ESP_ERROR_CHECK(rpr0521_init());
-
     int count = 0;
     while (count < 15)
     {
         uint16_t als_value;
-        ESP_ERROR_CHECK(read_als_value(&als_value));
-        printf("als value:%d\n", als_value);
+        esp_err_t ret = read_als_value(&als_value);
+        if (ret == ESP_OK)
+        {
+            ESP_LOGI(TAG, "ALS Reading: %d lux", als_value);
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Failed to read ALS value: %s", esp_err_to_name(ret));
+        }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         count++;
     }
-
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
     ESP_LOGI(TAG, "I2C de-initialized successfully");
 }
